b4e7599185cc488a837f39ebd7f41f9eab0f1ffe
sunzhzh_demo <sunzheng@hotmail.com>
2019-12-31 19:56:04 +0800

    diercigaibian

--- src/weniu.java
+++ src/weniu.java
@@ -2,5 +2,6 @@ public class weniu {
     public static void main(String[] args) {
         System.out.println("niubi");
         System.out.println("shhsh");
+        System.out.println("jhjh");
     }
 }
